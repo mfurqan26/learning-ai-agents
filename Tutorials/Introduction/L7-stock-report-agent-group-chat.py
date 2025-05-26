ef02fb184884a4147d5d0adc4d4ec930f25e5c0b
Muhammad Furqan <91328394+mfurqan26@users.noreply.github.com>
2025-05-26 09:24:31 +0300

    add agent definations

--- Tutorials/Introduction/L7-stock-report-agent-group-chat.py
+++ Tutorials/Introduction/L7-stock-report-agent-group-chat.py
@@ -87,6 +87,65 @@ groupchat_result = user_proxy.initiate_chat(
     message=task,
 )
 
+
+# Define group chat agents again
+user_proxy = ConversableAgent(
+    name="Admin",
+    system_message="Give the task, and send "
+    "instructions to writer to refine the blog post.",
+    code_execution_config=False,
+    llm_config=llm_config,
+    human_input_mode="ALWAYS",
+)
+
+planner = ConversableAgent(
+    name="Planner",
+    system_message="Given a task, please determine "
+    "what information is needed to complete the task. "
+    "Please note that the information will all be retrieved using"
+    " Python code. Please only suggest information that can be "
+    "retrieved using Python code. "
+    "After each step is done by others, check the progress and "
+    "instruct the remaining steps. If a step fails, try to "
+    "workaround",
+    description="Given a task, determine what "
+    "information is needed to complete the task. "
+    "After each step is done by others, check the progress and "
+    "instruct the remaining steps",
+    llm_config=llm_config,
+)
+
+engineer = AssistantAgent(
+    name="Engineer",
+    llm_config=llm_config,
+    description="Write code based on the plan "
+    "provided by the planner.",
+)
+
+writer = ConversableAgent(
+    name="Writer",
+    llm_config=llm_config,
+    system_message="Writer. "
+    "Please write blogs in markdown format (with relevant titles)"
+    " and put the content in pseudo ```md``` code block. "
+    "You take feedback from the admin and refine your blog.",
+    description="After all the info is available, "
+    "write blogs based on the code execution results and take "
+    "feedback from the admin to refine the blog. ",
+)
+
+executor = ConversableAgent(
+    name="Executor",
+    description="Execute the code written by the "
+    "engineer and report the result.",
+    human_input_mode="NEVER",
+    code_execution_config={
+        "last_n_messages": 3,
+        "work_dir": "coding",
+        "use_docker": False,
+    },
+)
+
 # create group chat with user policy on transition order
 groupchat = GroupChat(
     agents=[user_proxy, engineer, writer, executor, planner],

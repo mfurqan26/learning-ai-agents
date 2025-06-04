40be25238e80d428faf654263acf9f91fc6f85da
Muhammad Furqan <91328394+mfurqan26@users.noreply.github.com>
2025-06-04 10:25:54 +0300

    lesson 5 prompt template and resources for mcp

---
+++ Tutorials/Anthropic-MCP-Intro/L5-adding-prompt-resources.py
@@ -0,0 +1,10 @@
+# see the the new added resources and prompt generation functions in /mcp_project/research_server.py
+
+# chatbot.py is updated to list the new resources and prompt generation functions
+
+# To run the updated research server, navigate to the mcp_project directory:
+# cd L6/mcp_project
+# Activate the virtual environment:
+# source .venv/bin/activate
+# Run the chatbot:
+# uv run mcp_chatbot.py
